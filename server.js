7ab6cb45593e5ab8f960a177b02f79b2080201fd
Usenmfon <usenmfonuko@gmail.com>
2022-10-27 16:52:53 +0100

    get/profile completed

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { json } = require("express");
+const cors = require("cors");
+// const routes = require("./src/routes/profileRoute")
+
+const app = express();
+
+app.use(cors());
+
+app.use(json());
+
+// app.use("/", routes);
+
+app.get("/", function (req, res) {
+  let data = {
+    slackUsername: "usenmfonuko",
+    backend: true,
+    age: 26,
+    bio: "I'm a backend developer using nodejs and I love reading",
+  };
+  res.json(data);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is runing on port ${port}`);
+});
